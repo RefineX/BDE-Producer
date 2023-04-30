397ec0055dbab3d6c56cf372e12cfcb1a5e9f3a5
ankitparekh21698 <ankitparekh21698@gmail.com>
2023-04-29 20:26:51 -0400

    Tertiary Analysis

--- src/main/scala/Producer.scala
+++ src/main/scala/Producer.scala
@@ -5,15 +5,27 @@ object Producer {
     import java.util.Properties
     import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
     import org.apache.kafka.common.serialization.StringSerializer
+    import java.time.LocalDateTime
+    import java.time.format.DateTimeFormatter
+    import scala.util.Random
 
-    // Function to convert map to json string
-    def toJson(query: Any): String = query match {
-      case m: Map[String, Any] => s"{${m.map(toJson(_)).mkString(",")}}"
-      case t: (String, Any) => s""""${t._1}":${toJson(t._2)}"""
-      case ss: Seq[Any] => s"""[${ss.map(toJson(_)).mkString(",")}]"""
-      case s: String => s""""$s""""
-      case null => "null"
-      case _ => query.toString
+    // Function to generate random transaction data
+    def generateTransaction(): String = {
+      val transaction_id = Random.nextInt(1000000)
+      val account_id = Random.nextInt(200) + 1
+      val branch_id = Random.nextInt(10) + 1
+      val channel_id = Random.nextInt(4) + 1
+      val timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+      val transaction_amount = BigDecimal(Random.nextDouble() * 1000).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+
+      s"""{
+        "transaction_id": $transaction_id,
+        "account_id": $account_id,
+        "branch_id": $branch_id,
+        "channel_id": $channel_id,
+        "transaction_timestamp": "$timestamp",
+        "transaction_amount": $transaction_amount
+      }"""
     }
 
     // Initialize producer properties
@@ -27,28 +39,18 @@ object Producer {
     // Initialize producer
     val producer = new KafkaProducer[String, String](kafkaProducerProps)
 
-    // Open csv file
-    val src = io.Source.fromFile("MOCK_DATA.csv")
-    // Get lines from csv
-    val lines = src.getLines
-    // Get first line (header) and convert to array
-    val header = lines.take(1).next.split(",")
-
-    // Iterate through the rest of the lines (up to 10000)
-    for (row <- lines.take(10000)) {
-      // Convert line to array
-      val rowArr = row.split(",")
-      // Create map from header and line
-      val rowMap = header.zip(rowArr).toMap
-      // Send row map via the producer
-      producer.send(new ProducerRecord[String, String]("myTopic",rowMap("ip_address"),toJson(rowMap)))
-      println(s"Sent row of ${rowMap("ip_address")} to myTopic")
-      // Wait before sending next row
-      Thread.sleep(2000)
+    // Iterate through 10000 transactions
+    for (_ <- 1 to 10000) {
+      // Generate a random transaction
+      val transaction = generateTransaction()
+      // Send the transaction to Kafka
+      producer.send(new ProducerRecord[String, String]("transactions", transaction))
+      println(s"Sent transaction: $transaction")
+      // Wait before sending the next transaction
+      Thread.sleep(1000)
     }
 
-    // Close the producer and file once done
+    // Close the producer once done
     producer.close()
-    src.close()
   }
 }
