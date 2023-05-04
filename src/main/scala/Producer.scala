ad3cc5c23e3fc563f91a6dba9fdf2a1e6ca64289
RefineX <btechproj2020@gmail.com>
2023-05-03 21:48:46 -0400

    Sped up txn production

--- src/main/scala/Producer.scala
+++ src/main/scala/Producer.scala
@@ -1,7 +1,10 @@
+
+
 object Producer {
   def main(args: Array[String]): Unit = {
 
     // Import libraries
+    import java.time.temporal.ChronoUnit
     import java.util.Properties
     import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
     import org.apache.kafka.common.serialization.StringSerializer
@@ -10,12 +13,12 @@ object Producer {
     import scala.util.Random
 
     // Function to generate random transaction data
-    def generateTransaction(): String = {
+    def generateTransaction(currTime: LocalDateTime): String = {
       val transaction_id = Random.nextInt(1000000)
       val account_id = Random.nextInt(200) + 1
       val branch_id = Random.nextInt(10) + 1
       val channel_id = Random.nextInt(4) + 1
-      val timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+      val timestamp = currTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
       val transaction_amount = BigDecimal(Random.nextDouble() * 1000).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
 
       s"""{
@@ -36,18 +39,24 @@ object Producer {
       props.put("value.serializer", classOf[StringSerializer].getName)
       props
     }
+
     // Initialize producer
     val producer = new KafkaProducer[String, String](kafkaProducerProps)
-
+    var currTime = LocalDateTime.now()
     // Iterate through 10000 transactions
-    for (_ <- 1 to 10000) {
+    for (i <- 1 to 100000) {
+      // Random time delta
+      val delta = math.max((Random.nextGaussian()+1).toInt, 1)
+      currTime = currTime.plus(delta, ChronoUnit.MINUTES)
       // Generate a random transaction
-      val transaction = generateTransaction()
+      val transaction = generateTransaction(currTime)
       // Send the transaction to Kafka
       producer.send(new ProducerRecord[String, String]("transactions", transaction))
-      println(s"Sent transaction: $transaction")
+      if (i % 1000 == 0) {
+        println(s"Sent transaction: $transaction")
+      }
       // Wait before sending the next transaction
-      Thread.sleep(1000)
+      Thread.sleep(1)
     }
 
     // Close the producer once done
